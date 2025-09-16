1e4a21a7af7470c45b4eec1f484e3e3d4670d81e
Tarun Kumar <srfashions.tarun@gmail.com>
2025-09-16 23:32:04 +0530

    whatev playwright

--- testdriver/playwright_test.test.js
+++ testdriver/playwright_test.test.js
@@ -8,7 +8,7 @@ test('test', async ({ page }) => {
   await page.getByRole('textbox', { name: 'Image Link (optional)' }).fill('https://testdriverai.github.io/example-vite-recipe/pizza.jpg');
   await page.getByRole('button', { name: 'Add Recipe' }).click();
   await expect(page.getByRole('img', { name: 'Pizza' })).toBeVisible();
-await expect(page.locator('#root')).toContainText("PizzaIngredientsCheezetomatoesmushroomsInstructions:Put everything on top of the dough and cook for 10 minDelete Recipe");
+await expect(page.locator('#root')).toContainText("Recipe BookAdd RecipePizzaIngredients:CheezetomatoesmushroomsInstructions:Put everything on top of the dough and cook for 10 minDelete Recipe");
   await page.getByRole('button', { name: 'Delete Recipe' }).click();
   await expect(page.locator('#root')).toMatchAriaSnapshot(`
     - heading "Recipe Book" [level=1]
